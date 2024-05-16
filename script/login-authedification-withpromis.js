f08f08f97a0d5a0de063093516eaed66b09626be
jobinpjoseph707 <jobinpjoseph700@gmail.com>
2024-05-16 09:34:25 +0530

    final-signup and sign in

--- script/login-authedification-withpromis.js
+++ script/login-authedification-withpromis.js
@@ -1,5 +1,21 @@
+
+// Your Firebase configuration here
+var firebaseConfig = {
+  apiKey: "AIzaSyDSPmxx3T3NydlQlFwA5lfctSR-7vEonv8",
+  authDomain: "hexa-squad.firebaseapp.com",
+  databaseURL: "https://hexa-squad-default-rtdb.firebaseio.com",
+  projectId: "hexa-squad",
+  storageBucket: "hexa-squad.appspot.com",
+  messagingSenderId: "550225963727",
+  appId: "1:550225963727:web:81fa3b4bb3feae38d425ba",
+  measurementId: "G-LZXR9GNSH7"
+// ...
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 // Get a reference to the Firebase Realtime Database
 const database = firebase.database();
+
 
 // Function to get a reference to the user data in the database
 const getUserDataRef = () => {
@@ -17,15 +33,16 @@ const getUserDataRef = () => {
 };
 
 // Get the form element
-const form = document.querySelector('form');
+const form = document.querySelector('#login-form');
+
 
 // Add an event listener for form submission
 form.addEventListener('submit', async (event) => {
   event.preventDefault(); // Prevent form submission
 
   // Get the username and password values
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
 
   try {
     // Get a reference to the user data in the database
@@ -54,8 +71,7 @@ form.addEventListener('submit', async (event) => {
       // conflit vana nen nan kolum  - jobin
       // conflit varum..But we'll resolve dude.. All is well nn alle.. - Hari
       localStorage.setItem("username", username);
-
-
+      // window.location.href="loby.html"
 
     } else {
       console.log('Authentication failed');
